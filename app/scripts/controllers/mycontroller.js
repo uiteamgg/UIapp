8aa4810935d18678c8cefcf6a450ecc5b309d234
Koustubh <koustubh89@gmail.com>
2014-04-17 17:41:08 +0530

    Koustubh- Implemenmted NAvigation top bar

--- app/scripts/controllers/mycontroller.js
+++ app/scripts/controllers/mycontroller.js
@@ -51,8 +51,72 @@ angular.module('yoAngularProjectApp')
             }]
         });
     };
-    var values="";
-    myPieChart(values);
+
+    var values1="";
+    myPieChart(values1);
+
+    var myColumnChart= function (values) {
+    var arr2=[              
+            {rel:"sprint-1",series:[49, 71, 106, 129]},
+            {rel:"sprint-2",series:[83, 78, 98, 93]},
+            {rel:"sprint-3",series:[48, 38, 39, 41]},
+            {rel:"sprint-4",series:[42, 32, 34, 39]}
+            ];
+        $('.chart2').highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Sprint Release-wise'
+            },
+            subtitle: {
+                text: ''
+            },
+            xAxis: {
+                categories:['release-1','release-2','release-3',"release-4"]
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Stories'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: [{
+                name: arr2[0].rel,
+                data: arr2[0].series
+    
+            }, {
+                name: arr2[1].rel,
+                data: arr2[1].series
+    
+            }, {
+                name: arr2[2].rel,
+                data: arr2[2].series
+    
+            }, {
+                name: arr2[3].rel,
+                data: arr2[3].series
+    
+            }]
+        });
+};
+    var values2="";
+    myColumnChart(values2); 
+
   })
 
  .controller('HotelCtrl', function ($scope,Myservice) {
